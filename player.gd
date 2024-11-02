d6f60d660bf0521e2128f6b3ee1bbe77c07a8feb
Kevin Hoser <>
2024-11-02 13:12:56 -0700

    Clean up player script and add logging timer

--- player.gd
+++ player.gd
@@ -3,25 +3,33 @@ extends CharacterBody2D
 class_name Player
 
 signal sanityChanged
+signal player_death()
 
-@export var speed = 400
-var resource_count = 0
-@export var inventory_limit = 5
-var on_resource = null
-var on_base = null
-var distance_to_base = 0
 const DEFAULT_BASE_RADIUS = 500 # distance where player is considered near/far base
 const DEFAULT_SANITY_CAP = 100 # cap at which sanity can no longer be increased
 const DEFAULT_SANITY_LOSS = -1 # rate of sanity loss when away from base in sanity per second
 const DEFAULT_SANITY_GAIN = 10 # rate of sanity gain when new base in sanity per second
+const LOGGING_FREQ_MSEC = 1000 # Period in milliseconds between logging outputs
+
+@export var inventory_limit = 5
+@export var speed = 400
+
+var time_curr = 0
+var time_last_logged = 0
+
+var resource_count = 0
+var on_resource = null
+var on_base = null
+var distance_to_base = 0
+
+var maxSanity = DEFAULT_SANITY_CAP
+var sanity = DEFAULT_SANITY_CAP
 var sanity_loss_modifier = 0 # modifiers which increase or decrease the loss of sanity when away from base
 var sanity_gain_modifier = 0 # modifiers which increase or decrease sanity gain when near base
 var sanity_cap_modifier = 0 # modifiers which increase or decrease maximum sanity
 var base_redius_modifier = 0 # modifiers which increase or decrease the base's radius
-var maxSanity = DEFAULT_SANITY_CAP
-var sanity = DEFAULT_SANITY_CAP
+
 @onready var base = get_parent().get_node('Base')
-signal player_death()
 
 func update_resources():
 	if resource_count < inventory_limit:
@@ -49,11 +57,14 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func _process(delta):
+	time_curr = Time.get_ticks_msec()
+	
 	# Resource Calculations
 	if on_resource != null and Input.is_action_just_pressed("interact"):
 		update_resources()
 	if on_base != null and Input.is_action_just_pressed("interact"):
 		update_base()
+	
 	# Sanity Calculations
 	distance_to_base = base.global_position.distance_to(global_position)
 	maxSanity = DEFAULT_SANITY_CAP + sanity_cap_modifier
@@ -71,7 +82,11 @@ func _process(delta):
 		sanity = maxSanity
 	else:
 		emit_signal("sanityChanged")
-	print(str("Player distance and sanity: (", distance_to_base, ", ", sanity, ")"))
+	
+	# Logging
+	if time_curr - time_last_logged > LOGGING_FREQ_MSEC:
+		print(str("Player: (distance_to_base: ", distance_to_base, ", sanity:", sanity, ")"))
+		time_last_logged = time_curr
 
 func die() -> void:
 	print("Player is dead")
