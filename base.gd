9c4e1b7cf2ed3233ab3ed7f8055727bc75201f6a
Alex Schendel <alex.schendel@intel.com>
2024-11-02 00:02:13 -0700

    Add World, Base, and Resources
    This commit adds the world, base, and resources.
    Player is in charge of managing resource additions and deletions.
    World uses a TileMapLayer.
    
    Signed-off-by: Alex Schendel <alex.schendel@intel.com>

---
+++ base.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var resource_count = 0
+@onready var player = get_parent().get_node('Player')
+
+
+func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
+	player.on_base = self
+
+func _on_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	player.on_base = null
--- player.gd
+++ player.gd
@@ -1,11 +1,38 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var resource_count = 0
+@export var inventory_limit = 5
+var on_resource = null
+var on_base = null
+
+func update_resources():
+	if resource_count < inventory_limit:
+		on_resource.resource_count -= 1
+		resource_count += 1
+		print(str("Player now has ", resource_count, " resources."))
+		if on_resource.resource_count == 0:
+			print("Resource exhausted.")
+			on_resource.queue_free()
+	else:
+		print("Player's inventory is full.")
+		
+func update_base():
+	on_base.resource_count += resource_count
+	print(str("Player entered base with ", resource_count, " resources and dropped off resources."))
+	print("Base now has ", on_base.resource_count, " resources.")
+	resource_count = 0
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+func _process(_delta):
+	if on_resource != null and Input.is_action_just_pressed("interact"):
+		update_resources()
+	if on_base != null and Input.is_action_just_pressed("interact"):
+		update_base()
