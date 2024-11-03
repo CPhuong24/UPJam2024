0961c5f2be547cd8eca2f097d6112854363a23f2
Alex Schendel <alex.schendel@intel.com>
2024-11-03 12:20:31 -0800

    Add Continue button to Storyboard
    Removes timer-based transition from Storyboard, instead using a
    continue button.
    
    Signed-off-by: Alex Schendel <alex.schendel@intel.com>

--- fade_transition.gd
+++ fade_transition.gd
@@ -1,8 +1,7 @@
 extends CanvasLayer
 
-func _ready():
-	# Wait for 5 seconds, then start the fade-out
-	await get_tree().create_timer(9.0).timeout
+
+func _on_button_pressed() -> void:
 	$AnimationPlayer.play("fade_out")
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://world.tscn")
