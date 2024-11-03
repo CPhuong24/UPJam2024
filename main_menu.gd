2e2c796474ed946b3000dc0a0f7aadcd3e39fa2b
Steven Lee <leeste25@up.edu>
2024-11-03 09:57:14 -0800

    added controls page and a game title

--- main_menu.gd
+++ main_menu.gd
@@ -4,12 +4,11 @@ extends Control
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://story_board.tscn")
-	 # Replace with function body.
 
 
 func _on_how_to_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://controls.tscn")
 
 
 func _on_quit_pressed() -> void:
-	get_tree().quit() # Replace with function body.
+	get_tree().quit()
