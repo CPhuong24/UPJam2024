3d1e1c091db4f37a55a16952380230c6918e930e
Steven Lee <leeste25@up.edu>
2024-11-03 10:13:56 -0800

    minor storyboard changes

--- fade_transition.gd
+++ fade_transition.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 func _ready():
 	# Wait for 5 seconds, then start the fade-out
-	await get_tree().create_timer(10.0).timeout
+	await get_tree().create_timer(9.0).timeout
 	$AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://world.tscn")
